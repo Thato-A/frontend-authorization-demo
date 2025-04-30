5e4601171a0071a7f10ee230a9881014d9560ba5
Lily Anderson <thatoanderson92@gmail.com>
2025-04-30 09:02:07 -0400

    initial commit

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,8 +1,19 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
+import { removeToken } from "../utils/token";
 import Logo from "./Logo";
 import "./styles/NavBar.css";
+import { useContext } from "react";
+import AppContext from "../context/AppContext";
 
 function NavBar() {
+  const navigate = useNavigate();
+  const { setIsLoggedIn } = useContext(AppContext);
+
+  function signOut() {
+    removeToken();
+    navigate("/login");
+    setIsLoggedIn(false);
+  }
   return (
     <div className="navbar">
       <div className="navbar__logo">
@@ -20,7 +31,9 @@ function NavBar() {
           </NavLink>
         </li>
         <li>
-          <button className="navbar__link navbar__button">Sign Out</button>
+          <button className="navbar__link navbar__button" onClick={signOut}>
+            Sign Out
+          </button>
         </li>
       </ul>
     </div>
